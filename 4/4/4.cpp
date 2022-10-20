342fde512ef7e2978cce0c74df433d0aedb22285
gr.torgan <60741700+gr-torgan@users.noreply.github.com>
2022-10-20 19:07:34 +0500

    Add files via upload

---
+++ 4/4/4.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int n, i, f, k, del = 0;
+    int mas[100]{};
+    int mas2[100]{};
+    cout << "n = ";
+    cin >> n;
+    for (i = 0; i < n; i++) {
+        mas[i] = rand();
+        cout << mas[i] << " ";
+    }
+    cout << '\n';
+    cout << "del = ";
+    cin >> del;
+    for (i = 0; i < n; i++) {
+        if (del == mas[i])
+        {
+            for (f = i; f < n; f++)
+            {
+                mas2[f] = mas[f + 1];
+                mas[f] = mas2[f];
+            }
+            n--;
+            exit;
+        }
+    }
+    cout << "k = ";
+    cin >> k;
+    n = n + k;
+    for (i = 0; i < n; i++) {
+
+        mas2[i] = rand();
+    }
+    for (i = k; i < n; i++) {
+
+        mas2[i] = mas[i - k];
+    }
+    for (i = k; i < n; i++) {
+
+        mas[i] = mas2[i];
+    }
+    for (i = 0; i < n; i++) {
+
+        cout << mas[i] << " ";
+    }
+    return 0;
+    _getch;
+}
---
+++ 5/5/5.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+const int N = 10;
+int random(int N) { return rand() % N; }
+long lower_sum(int mat[N][N], size_t dim)
+{
+    long sum = 0;
+    for (size_t i = 0; i < dim; ++i)
+        for (size_t j = 0; j < i; ++j)
+            sum += mat[i][j];
+    return sum;
+}
+int main()
+{
+    int n, B[N][N], i, j, k, a = -10, b = 10, max = -10000;
+    printf("Vvedite kolichestvo matric\n");
+    scanf_s("%d", &n);
+    long* sum = new long[n];
+    for (k = 0; k < n; k++)
+    {
+        for (i = 0; i < N; i++)
+            for (j = 0; j < N; j++)
+                B[i][j] = random(b - a + 1) + a;
+        sum[k] = lower_sum(B, N);
+    }
+    for (i = 0; i < n; i++)
+        if (sum[i] > max) max = sum[i];
+    printf("max summa:%d\n", max);
+    _getch();
+    return 0;
+}
