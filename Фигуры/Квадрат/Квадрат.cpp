9574ad27ac08c1ac0bcf9ff27898a8a222c63f0f
gr.torgan <60741700+gr-torgan@users.noreply.github.com>
2022-10-20 19:11:54 +0500

    Add files via upload

---
+++ Фигуры/Квадрат/Квадрат.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int n = 0;
+    cin >> n;
+    for (int i=1;i<=n;i++)
+        cout << "*";
+    cout << "\n";
+    for (int i = 1; i <= (n - 2); i++)
+    {
+        cout << "*";
+        for (int j = 1; j <= (n - 2); j++)
+            cout << " ";
+        cout << "*";
+        cout << "\n";
+    }
+    for (int i = 1; i <= n; i++)
+        cout << "*";
+    _getch; //Для того что бы окно не закрылось
+    return 0;
+}
---
+++ Фигуры/Прямоугольный треугольник/Прямоугольный треугольник.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int n = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= (n-i); j++)
+            cout << " ";
+        for (int j = 1; j <= i; j++)
+            cout << "*";
+        cout << '\n';
+    }
+    _getch; //Для того что бы окно не закрылось
+    return 0;
+}
