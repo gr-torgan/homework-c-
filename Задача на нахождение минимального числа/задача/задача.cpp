2a3bb05cc455de0c4c750e8dee1c6fe9d8d924bf
gr.torgan <60741700+gr-torgan@users.noreply.github.com>
2022-10-21 18:33:54 +0500

    Add files via upload

---
+++ Задача на нахождение минимального числа/задача/задача.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <limits>
+
+int main() {
+    int a, min(std::numeric_limits<int>::max());
+    for (;;) {
+        std::cin >> a;
+        if (!a) break;
+        if (a < min) min = a;
+    }
+    std::cout << min;
+    return 0;
+}
